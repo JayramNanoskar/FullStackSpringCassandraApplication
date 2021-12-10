01019abb5c16fdc0b9777a19e03151ab9d5e0f87
JayramNanoskar <jayram.n978@gmail.com>
2021-12-11 00:18:12 +0530

    Initial commit | Adding project

---
+++ bookreads-data-loader/src/main/java/com/jayram/bookreadsdataloader/BookreadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.jayram.bookreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ bookreads-data-loader/src/test/java/com/jayram/bookreadsdataloader/BookreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayram.bookreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
