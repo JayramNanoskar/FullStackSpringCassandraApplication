19c43d2fb357e5b9de10cc999b96112a5fc437f8
JayramNanoskar <jayram.n978@gmail.com>
2021-12-11 13:20:29 +0530

    Connecting Spring Boot app to DataStax Astra

--- bookreads-data-loader/src/main/java/com/jayram/bookreadsdataloader/BookreadsDataLoaderApplication.java
+++ bookreads-data-loader/src/main/java/com/jayram/bookreadsdataloader/BookreadsDataLoaderApplication.java
@@ -1,13 +1,31 @@
 package com.jayram.bookreadsdataloader;
 
+import java.nio.file.Path;
+
+import com.jayram.bookreadsdataloader.connection.DataStaxAstraProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class) //To load the configuration
 public class BookreadsDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookreadsDataLoaderApplication.class, args);
+	}
+
+
+	//Exposes bean necessary for app to use the Astra secure bundle to connect to the database
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path path = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(path);
 	}
 
 }
