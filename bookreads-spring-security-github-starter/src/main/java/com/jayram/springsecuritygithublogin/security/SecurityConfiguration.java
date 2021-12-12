8c5a769d034ede86ea70aa7f8941dc18f003eb1d
JayramNanoskar <jayram.n978@gmail.com>
2021-12-12 20:02:25 +0530

    Adding Spring Security app for Auth setup

---
+++ bookreads-spring-security-github-starter/src/main/java/com/jayram/springsecuritygithublogin/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.jayram.springsecuritygithublogin.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+			.antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+			.and()
+            .csrf(c -> 
+            c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            )
+            .logout(l -> 
+            l.logoutSuccessUrl("/").permitAll()
+            )
+            .oauth2Login();
+    }
+}
